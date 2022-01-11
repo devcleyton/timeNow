750ba97630dafc46b1bebf86dbdda08cf4a5e7d2
devcleyton <cleyton.oliveira.dev@gmail.com>
2022-01-11 17:29:18 -0300

    commit

--- index.js
+++ index.js
@@ -1,48 +1,32 @@
-//WOEID CIDADES
-let url1 = "https://api.hgbrasil.com/weather?format=json-cors&woeid=455819"
-let url2 = "https://api.hgbrasil.com/weather?format=json-cors&woeid=455819"
+const url1 = "https://api.hgbrasil.com/weather?format=json-cors&woeid=455819&key=3b7f073a"
 
-function get1(url1) {
+function getUrl(url1) {
     let request = new XMLHttpRequest()
     request.open("GET", url1, false)
     request.send()
     return request.responseText
 }
 
-function get2(url2) {
-    let request = new XMLHttpRequest()
-    request.open("GET", url2, false)
-    request.send()
-    return request.responseText
-}
-
 
 function main() {
-    tempo = get1(url1);
-    tempo2 = get2(url2);
-    tempoJson = JSON.parse(tempo)
-    tempoJson2 = JSON.parse(tempo2)
+    tempo = getUrl(url1);
 
-    console.log(tempoJson)
-    console.log(tempoJson2)
+    tempoJson = JSON.parse(tempo)
 
     let secao = document.getElementById("secao")
 
     //Brasília
     let cidadeBrasilia = document.createElement("h1")
     let temperaturaBrasilia = document.createElement("p")
+    let descricaoBrasilia = document.createElement("p")
+
     cidadeBrasilia.innerHTML = tempoJson.results.city
     temperaturaBrasilia.innerHTML = tempoJson.results.temp + " graus"
+    descricaoBrasilia.innerHTML = tempoJson.results.forecast[0].description
+
     secao.appendChild(cidadeBrasilia)
     secao.appendChild(temperaturaBrasilia)
-
-    //Caldas
-    let cidadeCaldas = document.createElement("h1")
-    let temperaturaCaldas = document.createElement("p")
-    cidadeCaldas.innerHTML = tempoJson2.results.city
-    temperaturaCaldas.innerHTML = tempoJson2.results.temp + " graus"
-    secao.appendChild(cidadeCaldas)
-    secao.appendChild(temperaturaCaldas)
+    secao.appendChild(descricaoBrasilia)
 
 
 }
